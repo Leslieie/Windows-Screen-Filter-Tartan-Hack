11a6f9e3396400c07651dc4dbbb47e09cffc5fff
KevinL135 <luokevin135@gmail.com>
2026-02-06 23:59:13 -0500

    Glass window can be clicked through and follows a test window

--- main.js
+++ main.js
@@ -1,17 +1,46 @@
-const { app, BrowserWindow, desktopCapturer } = require('electron');
+const { app, BrowserWindow } = require('electron');
+
+let targetWin;
+let overlayWin;
 
 app.whenReady().then(() => {
-  const win = new BrowserWindow({
+  targetWin = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  targetWin.loadURL('https://example.com');
+
+  overlayWin = new BrowserWindow({
     transparent: true,
     frame: false,
     alwaysOnTop: true,
-    width: 800,
-    height: 600,
+    hasShadow: false,
+    focusable: false,
+    skipTaskbar: true,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
     }
   });
-  
-  win.loadFile('index.html');
+
+  overlayWin.loadFile('index.html');
+
+  // Click-through
+  overlayWin.setIgnoreMouseEvents(true, { forward: true });
+
+  const syncOverlay = () => {
+    const bounds = targetWin.getBounds();
+    overlayWin.setBounds(bounds);
+  };
+
+  // Initial sync
+  syncOverlay();
+
+  // Keep in sync
+  targetWin.on('move', syncOverlay);
+  targetWin.on('resize', syncOverlay);
+  targetWin.on('show', () => overlayWin.show());
+  targetWin.on('hide', () => overlayWin.hide());
+  targetWin.on('closed', () => overlayWin.close());
 });
